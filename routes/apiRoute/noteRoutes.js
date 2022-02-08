46602257475755fd84855553c99dd597abedae82
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-08 13:13:49 -0700

    route work, moving to branch

--- routes/apiRoute/noteRoutes.js
+++ routes/apiRoute/noteRoutes.js
@@ -1,28 +1,22 @@
 const router = require('express').Router();
+const { notes } = require('../../db/db.json');
 
-const {
-    notes
-} = require('../../db/db.json');
-
-const {
-    createNewNote,
-    deleteNote
-} = require('../../lib/noteFunctions');
+const { createNewNote, deleteNote } = require('../../lib/noteFunctions');
 
 router.get('/notes', (req, res) => {
-    let saved = notes;
-    res.json(saved);
-})
+    let results = notes;
+    res.json(results);
+});
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
-})
+});
 
 router.delete('/notes/:id', (req, res) => {
     deleteNote(notes, req.params.id);
     res.json(notes);
-})
+});
 
 module.exports = router;
