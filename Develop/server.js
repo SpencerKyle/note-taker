03a825285671aab6f93d769533e0ea60e8024a76
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-08 13:01:39 -0700

    fixed filestructure

--- Develop/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { notes } = require('./db/db.json')
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.listen(PORT, () => {
-    console.log(`WORKING ON PORT ${PORT}!`)
-});
